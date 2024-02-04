d60051ffed21050532eb8dbb7c956ce4d18a0323
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-02-04 19:15:26 +0530

    Initialize Calculator JavaScript functionality
    Added JavaScript code to initialize essential variables and select necessary elements for the calculator application. The script sets up event listeners for clear, equal, decimal buttons, operator buttons, and number buttons. Variables for the operator, previous value, and current value are initialized to facilitate future functionality.

---
+++ main.js
@@ -0,0 +1,15 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector("#btn-clear");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let operators = document.querySelectorAll(".operator");
+    let numbers = document.querySelectorAll(".numbers");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+});
